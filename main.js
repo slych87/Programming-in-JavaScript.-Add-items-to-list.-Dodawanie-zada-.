d0e6b73744bc7c309e18f1f96762e4694c019090
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-17 23:22:50 +0100

    end

---
+++ main.js
@@ -0,0 +1,29 @@
+const from = document.querySelector('form');
+const ul = document.querySelector('ul');
+const taskNumber = document.querySelector('h1 span');
+const listItems = document.getElementsByClassName('task');
+const input = document.querySelector('input');
+
+const removeTask = (e) => {
+    e.target.parentNode.remove();
+    taskNumber.textContent = listItems.length;
+}
+
+
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = input.value;
+    if (titleTask === "") return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + "<button>Usuń</button>";
+    ul.appendChild(task);
+    input.value = "";
+
+    // const liItems = document.querySelectorAll('li.task').length;
+    taskNumber.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask);
+
+}
+
+from.addEventListener('submit', addTask);
